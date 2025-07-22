90495f4e8439ccb2c3879cb351bf1bcb8df5e645
anusha918 <ga808410@gmail.com>
2025-07-22 13:58:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="center-container">
+      <h1>Hello React!</h1>
+      <p>You've clicked 0 times</p>
+      <button>Click me</button>
+    </div>
+  );
+}
+
+export default App;
+
+
